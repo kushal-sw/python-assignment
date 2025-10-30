37b8d61803176be939e015cc27687c2e310c93d2
kushal-sw <kushalswargam484@gmail.com>
2025-10-30 16:06:55 +0530

    Added all Python files

--- asssesment1.py
+++ asssesment1.py
@@ -4,3 +4,8 @@ if age < 18:
     print(f"Hello {name}, you are a minor.")
 else:
     print(f"Hello {name}, you are an adult.")
+
+
+x =5
+z =10
+print(x > 5 or x< 10)
---
+++ test.py
@@ -0,0 +1,11 @@
+x = int("257")
+y = int("257")
+
+a = int("3")
+b = int("3")
+
+print(x is y)
+print(a is b)
+print(x == y)
+
+print(a == b)
